fbe19491943a3bd9fced33eb2f0ecec521274b50
Zafer <zafertapikara1@gmail.com>
2023-08-25 09:02:58 +0200

    v1

--- test/pageobjects/secure.page.ts
+++ test/pageobjects/secure.page.ts
@@ -5,38 +5,42 @@ import Page from './page';
  * sub page containing specific selectors and methods for a specific page
  */
 class SecurePage extends Page {
-    /**
-     * define selectors using getter methods
-     */
- 
-    public get physicianName () {
-        return $('//h1[text()="Dr. Peter Test"]')
+
+     //   Get the physician's name from the profile section   
+     public get physicianName () {
+        return $('[class="text-wrap header__content--title"]')
     }
 
+    //  Get the physician's street and house number 
     public get street () {
-        return $('//p[text()=" Blücherstraße 10 "]')
+        return $('[data-automation="Address - Street House"]')
     }
 
- public get city () {
-        return $('//p[text()=" 56564 Neuwied "]')
+    // Get the physician's city
+    public get city () {
+        return $('[data-automation="Address - Postal city code"]')
     }
+
+    //  Get the openimg time in the afternoon
     public get startTimeAfternoon () {
-        return $('(//div[@class="text-day-hour__item-container current-date"] //div[@class="text-day-hour__text-startTime"])[2]')
+        return $('(//div[@class="text-day-hours-container__items current-date ng-star-inserted"] //div[@class="text-day-hour__text-startTime"])[2]')
     }
     
-
-public get endTimeAfternoon () {
-        return $('(//div[@class="text-day-hour__item-container current-date"] //div[@class="text-day-hour__text-endTime"])[2]')
+     //Get the closing time in the afternoon
+    public get endTimeAfternoon () {
+        return $('(//div[@class="text-day-hours-container__items current-date ng-star-inserted"] //div[@class="text-day-hour__text-endTime"])[2]')
     }
-
-public get startTimeBeforenoon () {
-        return $('(//div[@class="text-day-hour__item-container current-date"] //div[@class="text-day-hour__text-startTime"])[1]')
+    
+     //Get the opening time in the morning
+    public get startTimeBeforenoon () {
+        return $('(//div[@class="text-day-hours-container__items current-date ng-star-inserted"] //div[@class="text-day-hour__text-startTime"])[1]')
     }
-
+    
+     // Get the closing time in the morning
     public get endTimeBeforenoon () {
-        return $('(//div[@class="text-day-hour__item-container current-date"] //div[@class="text-day-hour__text-endTime"])[1]')
+        return $('(//div[@class="text-day-hours-container__items current-date ng-star-inserted"] //div[@class="text-day-hour__text-endTime"])[1]')
     }
-    
+    // Get the current day 
 public get currentDay () {
         return $('[class="text-day__item--text current-date"]')
     }
